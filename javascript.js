542f4b75bee396eb747ab400cff0fd676422fc6f
Nishanthini0306 <141767345+Nishanthini0306@users.noreply.github.com>
2024-07-23 17:15:52 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,22 @@
+function myFunction() {
+    document.getElementById("cont").style.backgroundColor = "red";
+    document.getElementById("span").style.backgroundColor = "red";
+    document.getElementById("span").textContent = "red";
+}
+function col() {
+    document.getElementById("cont").style.backgroundColor = "green";
+    document.getElementById("span").style.backgroundColor = "green";
+    document.getElementById("span").textContent = "green";
+}
+function color() {
+    document.getElementById("cont").style.backgroundColor = "lightseagreen";
+    document.getElementById("span").style.backgroundColor = "lightseagreen";
+    document.getElementById("span").textContent = "lightseagreen";
+}
+function col1() {
+    document.getElementById("cont").style.backgroundColor = "palevioletred";
+    document.getElementById("span").style.backgroundColor = "palevioletred";
+    document.getElementById("span").textContent = "palevioletred";
+}
+
+
